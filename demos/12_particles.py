2a1a0112fdf0efa788bb9f0fd91cac9212b729a7
gummbum <gummbum@localhost>
2015-11-01 00:40:08 -0700

    Some restructuring of Schedule and FixedStep methods. Prettying demo 12.

--- demos/12_particles.py
+++ demos/12_particles.py
@@ -1,7 +1,7 @@
 import random
 
 import gsdl2
-from gsdl2.locals import QUIT, KEYDOWN, S_ESCAPE, S_TAB, MOUSEMOTION
+from gsdl2.locals import QUIT, KEYDOWN, S_ESCAPE, S_TAB, S_I, MOUSEMOTION
 
 
 SCALE = 3.0
@@ -21,6 +21,8 @@ class Spark(gsdl2.particles.Particle):
         self.ay = 0
         self.alpha = 255
         self.lifespan = life
+        self.oldx = x
+        self.oldy = y
 
         if self.image is None:
             surf = gsdl2.Surface((2, 2))
@@ -32,6 +34,9 @@ class Spark(gsdl2.particles.Particle):
 
     def update(self, dt, world):
         self.alpha = abs(int(255 * self.life / self.lifespan))
+
+        self.oldx = self.x
+        self.oldy = self.y
 
         scale = SCALE
         vx = self.vx
@@ -45,6 +50,16 @@ class Spark(gsdl2.particles.Particle):
         self.y += vy * dt * scale
 
         self.rect.center = self.x, self.y
+
+    def get_rect_interpolated(self, interp):
+        """interpolate between the old and current position"""
+        x0 = self.oldx
+        x1 = self.x
+        y0 = self.oldy
+        y1 = self.y
+        rect = self.rect
+        rect.topleft = int(x0 + (x1 - x0) * interp), int(y0 + (y1 - y0) * interp)
+        return rect
 
 
 class Sparkler(object):
@@ -100,18 +115,25 @@ class Sparkler(object):
             p.image.set_alpha(p.alpha)
             renderer.copy(p.image, p.rect)
 
+    def render_interpolated(self, renderer, interp):
+        for p in self.particles:
+            p.image.set_alpha(p.alpha)
+            renderer.copy(p.image, p.get_rect_interpolated(interp))
+
 
 class Game(object):
     def __init__(self):
         self.screen = gsdl2.display.set_mode((640, 480))
-        gsdl2.display.set_caption("12_particles.py - ")
+        gsdl2.display.set_caption("12_particles.py - Ooo sparklies")
         self.screen_rect = self.screen.get_rect()
         self.renderer = gsdl2.display.get_window().create_renderer()
+
+        self.which_interval = [30, 60, 120, 240, 360, 15]
         self.clock = gsdl2.time.FixedDriver(self.update, 1.0 / 15.0)
-        self.which_fps = [30, 60, 120, 240, 360, 15]
-        self.fps = 15
-        self.elapsed = 1.0
         self.clock.new_schedule(self.caption, 1.0)
+        self.renderer_schedule = self.clock.new_schedule(self.draw, 0.0, keep_history=True)
+
+        self.do_interpolation = True
 
         self.mousex, self.mousey = self.screen_rect.center
         self.sparkler = Sparkler(
@@ -124,7 +146,18 @@ class Game(object):
             self.clock.tick()
 
     def update(self, dt):
-        self.elapsed -= dt
+        self.handle_events()
+        self.sparkler.update(dt, self.mousex, self.mousey)
+
+        # Interpolation can be toggled on/off. This is done by enabling/disabling self.renderer_schedule, because if it
+        # is off there is no point in spamming self.draw. In this case we want self.update and self.draw to be in
+        # lockstep, so we need to call self.draw from self.update. This is nicer than running separate update and draw
+        # schedules that are not in lockstep: we would often get two updates in a row and two draws in a row and it
+        # would look very jerky.
+        if not self.do_interpolation:
+            self.draw(None)
+
+    def handle_events(self):
         for e in gsdl2.event.get():
             if e.type == QUIT:
                 self.running = False
@@ -132,27 +165,39 @@ class Game(object):
                 if e.scancode == S_ESCAPE:
                     self.running = False
                 elif e.scancode == S_TAB:
-                    self.fps = self.which_fps.pop(0)
-                    self.which_fps.append(self.fps)
-                    self.clock.change_master(1.0 / self.fps, schedules_too=True)
+                    # Advance ticks per second setting
+                    per_second = self.which_interval.pop(0)
+                    self.which_interval.append(per_second)
+                    self.clock.change(1.0 / per_second)
+                elif e.scancode == S_I:
+                    # Toggle interpolation
+                    self.do_interpolation = not self.do_interpolation
+                    if self.do_interpolation:
+                        self.renderer_schedule.set_running(True)
+                    else:
+                        self.renderer_schedule.set_running(False)
             elif e.type == MOUSEMOTION:
                 self.mousex, self.mousey = e.pos
 
-        self.sparkler.update(dt, self.mousex, self.mousey)
-
+    def draw(self, sched):
         self.renderer.clear()
-        self.sparkler.render(self.renderer)
+        if self.do_interpolation:
+            interp = sched.interp
+            self.sparkler.render_interpolated(self.renderer, interp)
+        else:
+            self.sparkler.render(self.renderer)
         self.renderer.present()
 
     def caption(self, sched):
-        if self.elapsed <= 0.0:
-            self.elapsed += 1.0
-            gsdl2.display.set_caption(
-                'FPS: real={real} target={target} | Dt: {dt:0.3f} | Particles: {particles}'.format(
-                    real=int(self.clock.per_second()),
-                    target=self.fps,
-                    dt=self.clock.period,
-                    particles=len(self.sparkler.particles)))
+        dt = self.clock.period
+        target = 1.0 / dt
+        gsdl2.display.set_caption(
+            'Master: real={real}/s target={target}/s | FPS: {fps}/s | Particles: {particles} | Interp: {interp}'.format(
+                real=int(self.clock.per_second()),
+                target=int(target),
+                fps=int(self.renderer_schedule.per_second()),
+                particles=len(self.sparkler.particles),
+                interp=self.do_interpolation))
 
 
 def main():
