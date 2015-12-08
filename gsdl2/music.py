7c7e4b7de24a8e6e6a929a92bba18d44c862cb4b
gummbum <gummbum@localhost>
2015-12-08 15:51:55 -0800

    Fixes, enhancements, completed TODOs during the development of the shooter POC.

--- gsdl2/music.py
+++ gsdl2/music.py
@@ -1,28 +1,52 @@
-from .sdllibs import mixer_lib
+from .sdllibs import sdl_lib, mixer_lib
+from .sdlffi import sdl_ffi
 from .locals import utf8
+from .sdlconstants import SDL_INIT_AUDIO
+
+import event
 
 
 class _globals:
-
+    # TODO: this needs better logic, and probably a thread to monitor the queue
+    current_name = ''
     current = None
-    queue = None
-    channel = None
+    queued = None
+    end_event = None
+
+
+def get_init():
+    return sdl_lib.SDL_WasInit(SDL_INIT_AUDIO)
+
+
+# EXPERIMENTAL: enabled callback for pypy 4.0.0
+# https://cffi.readthedocs.org/en/latest
+#
+if True:
+    @sdl_ffi.callback('void (*)()')
+    def _music_finished():
+        if _globals.queued is not None:
+            load(_globals.queued)
+            _globals.queued = None
+            play()
+        if _globals.end_event:
+            event.post(event.Event(_globals.end_event, data1=_globals.current_name))
+        # TODO: post an event if configured on the Channel
+    mixer_lib.Mix_HookMusicFinished(_music_finished)
 
 
 def load(filename):
     if get_busy():
         stop()
     _globals.current = mixer_lib.Mix_LoadMUS(utf8(filename))
-    _globals.queue = None
+    _globals.current_name = filename
     set_volume(1.0)
 
 
 def play(loops=0, start=0.0):
-    if _globals.current:
-        _globals.channel = None
+    if _globals.current is not None:
+        mixer_lib.Mix_PlayMusic(_globals.current, loops)
         if start > 0.0:
             set_pos(start)
-        _globals.channel = mixer_lib.Mix_PlayMusic(_globals.current, loops)
 
 
 def rewind():
@@ -78,20 +102,22 @@ def get_pos(pos):
 
 
 def queue(filename):
-    # TODO: this needs some auto-polling method
-    if _globals.current is None:
-        load(filename)
-        play()
+    # TODO: this needs better logic and some auto-polling method
+    current = _globals.current
+    if current is None:
+        _globals.current = mixer_lib.Mix_LoadMUS(utf8(filename))
     else:
-        _globals.queue = mixer_lib.Mix_LoadMUS(utf8(filename))
+        # _globals.queued = mixer_lib.Mix_LoadMUS(utf8(filename))
+        _globals.queued = utf8(filename)
+    if not get_busy():
+        play()
 
 
 def set_endevent(type_):
-    # TODO: see queue()
-    pass
+    _globals.end_event = type_
 
 
 def get_endevent():
-    # return type_
-    # TODO: see queue()
-    pass
+    return _globals.end_event
+
+import mixer
