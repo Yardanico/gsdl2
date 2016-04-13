5eb393c03568ba3f1e3e44c45f6f90bc40093212
gummbum <gummbum@localhost>
2016-04-12 18:37:20 -0700

    Added load_texture()

--- gsdl2/image.py
+++ gsdl2/image.py
@@ -1,15 +1,24 @@
 from .sdllibs import image_lib
 from .surface import Surface
+from .texture import Texture
+from .display import get_renderer
 from .locals import utf8
 
 
-___all___ = ['load']
+___all___ = ['load', 'load_texture']
 
 
 def load(name):
-    img = image_lib.IMG_Load(utf8(name))
-    surf = Surface((img.w, img.h), surface=img)
+    sdl_surface = image_lib.IMG_Load(utf8(name))
+    surf = Surface((sdl_surface.w, sdl_surface.h), surface=sdl_surface)
     return surf
+
+
+def load_texture(name):
+    sdl_renderer = get_renderer().sdl_renderer
+    sdl_texture = image_lib.IMG_LoadTexture(sdl_renderer, utf8(name))
+    texture = Texture(sdl_renderer, sdl_texture=sdl_texture)
+    return texture
 
 
 def save(surface, name):
