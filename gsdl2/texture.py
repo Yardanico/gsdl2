1c2f96b75ae4da7151142a205b57074b4ec59f5c
gummbum <gummbum@localhost>
2016-04-13 21:35:35 -0700

    removed print statements

--- gsdl2/texture.py
+++ gsdl2/texture.py
@@ -17,23 +17,23 @@ log = logging.getLogger(__name__)
 class Texture(object):
     def __init__(self, renderer, surface=None, size=None, sdl_texture=None):
         if surface:
-            print('texture from surface')
+            # print('texture from surface')
             self.__sdl_texture = sdl_lib.SDL_CreateTextureFromSurface(renderer.sdl_renderer, surface.sdl_surface)
             self.__size = surface.get_size()
         elif sdl_texture:
-            print('texture from sdl texture')
+            # print('texture from sdl texture')
             self.__sdl_texture = sdl_texture
-            print(self.query())
+            # print(self.query())
             format, access, w, h = self.query()
             self.__size = w, h
         else:
-            print('texture from scratch')
-            print('SDL_TEXTUREACCESS_TARGET', sdl_lib.SDL_TEXTUREACCESS_TARGET)
+            # print('texture from scratch')
+            # print('SDL_TEXTUREACCESS_TARGET', sdl_lib.SDL_TEXTUREACCESS_TARGET)
             self.__sdl_texture = sdl_lib.SDL_CreateTexture(
                 renderer.sdl_renderer,
                 sdlpixels.SDL_PIXELFORMAT_ARGB8888,
                 sdl_lib.SDL_TEXTUREACCESS_TARGET, *size)
-            print(self.query())
+            # print(self.query())
             self.__size = tuple(size)
 
     def query(self):
