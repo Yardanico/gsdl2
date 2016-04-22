e11daaa244467351e44df647332b47ccffd8eb4f
gummbum <gummbum@localhost>
2016-04-22 00:58:54 -0700

    Added sanity checks and raise SDLException for null returns

--- gsdl2/image.py
+++ gsdl2/image.py
@@ -1,4 +1,4 @@
-from .sdllibs import image_lib
+from . import sdllibs
 from .surface import Surface
 from .texture import Texture
 from .display import get_renderer
@@ -9,17 +9,22 @@ ___all___ = ['load', 'load_texture']
 
 
 def load(name):
-    sdl_surface = image_lib.IMG_Load(utf8(name))
+    sdl_surface = sdllibs.image_lib.IMG_Load(utf8(name))
+    if sdl_surface == sdllibs.sdl_ffi.NULL:
+        raise sdllibs.SDLError()
     surf = Surface((sdl_surface.w, sdl_surface.h), surface=sdl_surface)
     return surf
 
 
 def load_texture(name):
-    sdl_renderer = get_renderer().sdl_renderer
-    sdl_texture = image_lib.IMG_LoadTexture(sdl_renderer, utf8(name))
-    texture = Texture(sdl_renderer, sdl_texture=sdl_texture)
+    renderer = get_renderer()
+    sdl_renderer = renderer.sdl_renderer
+    sdl_texture = sdllibs.image_lib.IMG_LoadTexture(sdl_renderer, utf8(name))
+    if sdl_texture == sdllibs.sdl_ffi.NULL:
+        raise sdllibs.SDLError()
+    texture = Texture(renderer, sdl_texture=sdl_texture)
     return texture
 
 
 def save(surface, name):
-    image_lib.IMG_SavePNG(surface.sdl_surface, utf8(name))
+    sdllibs.image_lib.IMG_SavePNG(surface.sdl_surface, utf8(name))
