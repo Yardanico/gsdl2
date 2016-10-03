f7a301edcaaab9d430d9d552eec8a8fb4186627c
Daniil Yarancev <tiberiumk12@gmail.com>
2016-10-03 20:35:46 +0300

    add pysdl2-cffi to setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ from distutils.core import setup
 setup(name='gsdl2',
       version='0.1',
       description='pygame-compatible wrapper to pysdl2-cffi',
-      author='Daniil Yarancev',
+      author='Daniil Yarancev and ',
       author_email='tiberiumk12@gmail.com',
       url='https://github.com/TiberiumPY/gsdl2',
       packages=['gsdl2'],
+      requires=['pysdl2-cffi']
      )
